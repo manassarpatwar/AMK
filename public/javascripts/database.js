6ab681ce116099ea6d11062d2ca700935bee7603
manassarpatwar <manas.mahesh.sarpatwar@gmail.com>
2021-03-14 22:52:53 +0530

    Now stores image in indexeddb as blob and loads cached image if user connected to visited room

---
+++ public/javascripts/database.js
@@ -0,0 +1,80 @@
+////////////////// DATABASE //////////////////
+// the database receives from the server the following structure
+import * as idb from './idb/index.js';
+
+let db;
+
+const IMAGE_DB_NAME= 'AMK_DB';
+const IMAGE_STORE_NAME= 'AMK_DB_STORE';
+
+/**
+ * it inits the database
+ */
+async function initDatabase(){
+    if (!db) {
+        db = await idb.openDB(IMAGE_DB_NAME, 2, {
+            upgrade(upgradeDb, oldVersion, newVersion) {
+                if (!upgradeDb.objectStoreNames.contains(IMAGE_STORE_NAME)) {
+                    let AMK_DB = upgradeDb.createObjectStore(IMAGE_STORE_NAME, {
+                        keyPath: 'id',
+                        autoIncrement: true
+                    });
+                    AMK_DB.createIndex('room', 'room', {unique: false});
+                }
+            }
+        });
+        console.log('db created');
+    }
+}
+window.initDatabase= initDatabase;
+/**
+ * it saves the image of a room in localStorage
+ * @param room
+ * @param image
+ */
+async function storeCachedData(room, image) {
+    console.log('inserting: ');
+    if (!db)
+        await initDatabase();
+    if (db) {
+        try{
+            let tx = await db.transaction([IMAGE_STORE_NAME], "readwrite");
+            let store = await tx.objectStore(IMAGE_STORE_NAME);
+            await store.put({room, image});
+            await tx.complete;
+            console.log('added item to the store! ');
+        } catch(error) {
+            localStorage.setItem(room, JSON.stringify(image));
+        };
+    }
+    else localStorage.setItem(room, JSON.stringify(image));
+}
+window.storeCachedData= storeCachedData;
+
+/**
+ * it retrieves the image stored for the specific room from the database
+ * @param room
+ * @returns {*}
+ */
+async function getCachedData(room) {
+    if (!db)
+        await initDatabase();
+    if (db) {
+        try {
+            console.log('fetching: ' + room);
+            let tx = await db.transaction(IMAGE_STORE_NAME, 'readonly');
+            let store = await tx.objectStore(IMAGE_STORE_NAME);
+            let index = await store.index('room');
+            let roomData = await index.get(IDBKeyRange.only(room));
+            await tx.complete;
+            return roomData
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        const roomData = localStorage.getItem(room);
+        
+        return roomData;
+    }
+}
+window.getCachedData= getCachedData;
