e68962d37947d30fa8746c5e5fb97bac69c77b4e
Aleksandra Kulbaka <awkulbaka1@sheffield.ac.uk>
2021-03-19 23:26:35 +0100

    get method template for a room  added

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Image Browsing' });
 });
 
+// To create a new room
 // in index.ejs in submit button redirect to route with the room number?
 //  but that would require rewriting socket.io and .ejs files quite drastically I think?
 router.post('/chats/:room', function(req, res, next) {
@@ -18,5 +19,14 @@ router.post('/chats/:room', function(req, res, next) {
 
 });
 
+// To join the existing room
+router.get('/chats/:room', function(req, res, next) {
+  var room= req.params.room;
+  // get image and name of that person from local database?
+
+  // need to create chat.ejs
+  res.render('chat', { room: room, image: img, name: name });
+
+});
 
 module.exports = router;
