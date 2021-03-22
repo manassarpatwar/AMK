d04503abf522f8d749eb65246f3eaeffc4dc8345
manassarpatwar <manas.mahesh.sarpatwar@gmail.com>
2021-03-22 16:16:50 +0530

    swapImages fix

--- public/javascripts/swapImages.js
+++ public/javascripts/swapImages.js
@@ -7,7 +7,10 @@ async function swapImage() {
     let roomNo = document.getElementById('roomNo').textContent.trim();
     let name = document.getElementById('user').textContent.trim();
     const imageBase64 = document.getElementById('image_base_64');
-    const new_image = {url: imageBase64.getAttribute("url"), base64: imageBase64.value};
-    getCachedData(room).then(data => {data.image = new_image; updateCachedData (data)}).then(location.assign('/chat/'+roomNo+'/'+name));
+    const newImage = {url: imageBase64.getAttribute("url"), base64: imageBase64.value};
+    getCachedData(roomNo).then(data => {
+        data.image = newImage; 
+        updateCachedData (data);
+    }).then(location.assign('/chat/'+roomNo+'/'+name));
 
 }
