0518673b181e286f25abc8d125c75e7dbbe7c415
Aleksandra Kulbaka <awkulbaka1@sheffield.ac.uk>
2021-05-19 02:18:21 +0100

    create folder and basic database configuration

---
+++ controllers/images.js
@@ -0,0 +1,29 @@
+
+const Image = require('../models/images');
+
+exports.insert = function (req, res) {
+    let userData = req.body;
+    if (userData == null) {
+        res.status(403).send('No data sent!')
+    }
+    try {
+        let image = new Image({
+            title: userData.title,
+            author: userData.author,
+            description: userData.description,
+            img: userData.img
+        });
+        console.log('received: ' + image);
+
+        image.save(function (err, results) {
+            console.log(results._id);
+            if (err)
+                res.status(500).send('Invalid data!');
+
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(image));
+        });
+    } catch (e) {
+        res.status(500).send('error ' + e);
+    }
+}
