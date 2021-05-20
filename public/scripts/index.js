41b2686b02ca51b5a506a3444095f9001c02b363
Aleksandra Kulbaka <awkulbaka1@sheffield.ac.uk>
2021-05-20 15:58:17 +0100

    Implemented basic knowledge graph

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -7,6 +7,8 @@ let img_title = null;
 let description = null;
 let chat= io.connect('/chat');
 
+const service_url = 'https://kgsearch.googleapis.com/v1/entities:search';
+const apiKey= 'AIzaSyAG7w627q-djB4gTTahssufwNOImRqdYKM';
 
 
 
@@ -364,4 +366,69 @@ function showOfflineWarning(){
 function hideOfflineWarning(){
     if (document.getElementById('offline_div')!=null)
         document.getElementById('offline_div').style.display='none';
+}
+
+
+/**
+ * it inits the widget by selecting the type from the field myType
+ * and it displays the Google Graph widget
+ * it also hides the form to get the type
+ */
+function widgetInit(){
+    let type= document.getElementById("myType").value;
+    if (type) {
+        let config = {
+            'limit': 10,
+            'languages': ['en'],
+            'types': [type],
+            'maxDescChars': 100,
+            'selectHandler': selectItem,
+        }
+        KGSearchWidget(apiKey, document.getElementById("myInput"), config);
+        document.getElementById('typeSet').innerHTML= 'of type: '+type;
+        document.getElementById('widget').style.display='block';
+        document.getElementById('typeForm').style.display= 'none';
+    }
+    else {
+        alert('Set the type please');
+        document.getElementById('widget').style.display='none';
+        document.getElementById('resultPanel').style.display='none';
+        document.getElementById('typeSet').innerHTML= '';
+        document.getElementById('typeForm').style.display= 'block';
+    }
+}
+
+/**
+ * callback called when an element in the widget is selected
+ * @param event the Google Graph widget event {@link https://developers.google.com/knowledge-graph/how-tos/search-widget}
+ */
+function selectItem(event){
+    let row= event.row;
+    // document.getElementById('resultImage').src= row.json.image.url;
+    document.getElementById('resultId').innerText= 'id: '+row.id;
+    document.getElementById('resultName').innerText= row.name;
+    document.getElementById('resultDescription').innerText= row.rc;
+    document.getElementById("resultUrl").href= row.qc;
+    document.getElementById('resultPanel').style.display= 'block';
+}
+
+/**
+ * currently not used. left for reference
+ * @param id
+ * @param type
+ */
+function queryMainEntity(id, type){
+    const  params = {
+        'query': mainEntityName,
+        'types': type,
+        'limit': 10,
+        'indent': true,
+        'key' : apiKey,
+    };
+    $.getJSON(service_url + '?callback=?', params, function(response) {
+        $.each(response.itemListElement, function (i, element) {
+
+            $('<div>', {text: element['result']['name']}).appendTo(document.body);
+        });
+    });
 }
