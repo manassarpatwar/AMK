1266ac28edf3e4fca1852eb86efd79c2f482fcdb
manassarpatwar <manas.mahesh.sarpatwar@gmail.com>
2021-05-21 08:04:01 +0530

    Init swagger

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Home page
+ *     responses:
+ *       200:
+ *         description: Renders the home page.
+ */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Image Browsing' });
 });
 
 /* GET chat page. */
+/**
+ * @swagger
+ * /chat/:room/:user
+ *   get:
+ *     description: Chat with username
+ *     responses:
+ *       200:
+ *         description: Renders the chat page with a username.
+ */
 router.get('/chat/:room/:user', function(req, res, next) {
   let room = req.params.room;
   let user = req.params.user;
@@ -14,6 +33,15 @@ router.get('/chat/:room/:user', function(req, res, next) {
 });
 
 /* GET chat page. */
+/**
+ * @swagger
+ * /chat/:room
+ *   get:
+ *     description: Anonymous Chat
+ *     responses:
+ *       200:
+ *         description: Renders the chat page with an anonymous username.
+ */
 router.get('/chat/:room', function(req, res, next) {
   var room = req.params.room;
   console.log('all');
